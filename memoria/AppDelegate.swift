cbe6713b66d5e26362c86e5513ae0cc893f01958
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2016-02-09 01:18:54 -0600

    listo

--- memoria/AppDelegate.swift
+++ memoria/AppDelegate.swift
@@ -1,46 +1,21 @@
-//
-//  AppDelegate.swift
-//  memoria
-//
-//  Created by Jose Andres Ceballos Vadillo on 09/02/16.
-//  Copyright © 2016 andres. All rights reserved.
-//
+//: Playground - noun: a place where people can play
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+var numeros = 0...100
 
-    var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+for i in numeros{
+    if i % 5 == 0 {
+        print("\(i)\t bingo")
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    if i % 2 == 0{
+        print("\(i)\t par")
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    if i % 2 != 0{
+        print("\(i)\t impar")
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    if (i >= 30) && (i <= 40){
+        print("\(i)\t Viva swift")
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
